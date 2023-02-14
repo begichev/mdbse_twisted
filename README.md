feed81066887b5e75a36d1a8e11b786ce5c9ed81
begichev <begichev.iv@phystech.edu>
2023-02-15 02:35:12 +0300

    modified interaction: now k grid in units of 2pi/a and integration over singularity performed over parallelogram

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
+fork of
+"""
 # Solution of Bethe-Salpeter equation for excitons in the 2D massive Dirac model
 
 Authors: Ya. V. Zhumagulov and D. R. Gulevich
-
+""" project 
+to calculate moire excitons in twisted MX2. fork author I. Begichev
 
 
--- mdirac/interaction.py
+++ mdirac/interaction.py
@@ -4,29 +4,25 @@ import matplotlib.pyplot as plt
 from scipy import special
         
 class Keldysh():
-    def __init__(self,epseff,r0):
+    def __init__(self,epseff,r0,kstep0):
         self.epseff=epseff
         self.r0=r0
-
-    def rpotential(self,absr):
-        factor=np.pi/(2.*self.epseff*self.r0)
-        rho=absr/self.r0
-        return factor*(special.struve(0,rho)-special.yn(0,rho))
+        self.kstep0=kstep0
     
     def kpotential(self,absk):
-        factor=2.*math.pi/self.epseff
+        factor=np.sqrt(3)/(2*self.eps)*(self.kstep0)**2
         return factor/(absk*(1.+self.r0*absk))
         
     # Average value of kpotential in kmesh cell at k=0, ignoring self.r0
+    # integral 1/q over hex with side=1 = 3*sqrt(3)*log(3)
+
     def WK00(self,kmesh):
         #factor=8.*math.pi # circle radius dp/2
-        factor=12.*math.pi*math.log(3.)/math.sqrt(3.) # integral over hexagon dp/sqrt(3.)
-        return factor/(kmesh.dp*self.epseff) 
+        # integral = 3*np.log(3) hexagon 
+        integral = 4.254 # parallelogram
+        factor = integral/self.eps*self.kstep0
+        return factor
     
-    # Average value of rpotential in rmesh cell at r=0
-    def WR00(self,rmesh):
-        WR00 = -(0.0772+math.log(0.25*rmesh.dp/self.r0))/(self.epseff*self.r0)
-        return WR00
     
 def pairs(subdomain,Wk):
     W=np.empty((subdomain.Np,subdomain.Np),dtype=np.float64)
@@ -35,27 +31,9 @@ def pairs(subdomain,Wk):
             W[ij0,ij1]=Wk[i1-i0,j1-j0]
     return W
 
-def bypairs(Wk,inds1,inds2):
-    W=np.empty((inds1[0].size,inds2[0].size),dtype=np.float64)
-    for ij0,(i0,j0) in enumerate(zip(*inds1)):
-        for ij1,(i1,j1) in enumerate(zip(*inds2)):
-            W[ij0,ij1]=Wk[i1-i0,j1-j0]
-    return W
-
-def sample_to_mesh(interaction,mesh,quadrature=None):
-    if quadrature==None:
-        kmesh=mesh
-        absk=np.linalg.norm(kmesh.p,axis=2)
-        WK=interaction.kpotential(absk)               
-        WK[0,0] = interaction.WK00(kmesh)
-        WK *= kmesh.vcell/(2.*math.pi)**2 
-    elif quadrature=='fft':
-        rmesh=mesh
-        Rnorm = rmesh.norm(center=(0,0))            
-        Wr = interaction.rpotential(Rnorm)
-        Wr[0,0] = interaction.WR00(rmesh)
-        WK=np.fft.fft2(Wr).real/rmesh.N1**2  # rmesh.p[0,0]=0., otherwise nonzero phase
-    else:
-        print('Unknown quadrature.')
-        WK=np.zeros((mesh.N1,mesh.N1))
+def sample_to_mesh(interaction,mesh):
+    kmesh=mesh
+    absk=np.linalg.norm(kmesh.p,axis=2)
+    WK=interaction.kpotential(absk)               
+    WK[0,0] = interaction.WK00(kmesh)
     return WK
